55a55ff047a43f4cc4b3bd26c22a19114c9c433d
ValentinaMarinG <valentinamaringalvis28@gmail.com>
2023-05-04 21:26:33 -0500

    Ready the connection with the mongobd database and ready the registration, login and creation of the token

--- parqueadero-uam-server/constants.js
+++ parqueadero-uam-server/constants.js
@@ -1,4 +1,20 @@
-API_VERSION = "v1";
-IP_SERVER = "localhost";
-PORT_DB = 4040;
-module.exports={API_VERSION, IP_SERVER, PORT_DB};
+const DB_HOST = "cluster0.vc0gl8f.mongodb.net";
+const DB_USER = "valentinamaring";
+const DB_PASSWORD = "Valentina.M128G";
+const DB_NAME = "software_db";
+
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const JWT_SECRET_KEY = "acceso-sistema";
+
+
+module.exports={
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    API_VERSION, 
+    IP_SERVER,
+    JWT_SECRET_KEY
+};
